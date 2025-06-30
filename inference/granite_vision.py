660776f0345cc26434014dbbd035485c1f83f724
root <root@guell.cs.ucl.ac.uk>
2025-06-30 11:01:45 +0000

    Testing image inference pipeline

---
+++ inference/granite_vision.py
@@ -0,0 +1,36 @@
+from transformers import AutoProcessor, AutoModelForVision2Seq
+from huggingface_hub import hf_hub_download
+import torch
+
+device = "hpu"
+
+model_path = "ibm-granite/granite-vision-3.3-2b"
+processor = AutoProcessor.from_pretrained(model_path)
+model = AutoModelForVision2Seq.from_pretrained(model_path).to(device)
+
+# prepare image and text prompt, using the appropriate prompt template
+
+img_path = hf_hub_download(repo_id=model_path, filename='example.png')
+
+conversation = [
+    {
+        "role": "user",
+        "content": [
+            {"type": "image", "url": img_path},
+            {"type": "text", "text": "What is the highest scoring model on ChartQA and what is its score?"},
+        ],
+    },
+]
+inputs = processor.apply_chat_template(
+    conversation,
+    add_generation_prompt=True,
+    tokenize=True,
+    return_dict=True,
+    return_tensors="pt"
+).to(device)
+
+
+# autoregressively complete prompt
+output = model.generate(**inputs, max_new_tokens=100)
+print(processor.decode(output[0], skip_special_tokens=True))
+
